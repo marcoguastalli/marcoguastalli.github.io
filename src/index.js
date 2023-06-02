21760f79212e34cc869f83f326907dfa3ee59ad2
marcoguastalli <marco.guastalli@gmail.com>
2023-06-02 21:21:39 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const init = async () => {
 
     const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    };
     let result = [];
+    console.log(`Fetch url: ${url}`);
     const response = await fetch(url, options)
         .then((response) => {
-            console.log(' Response status code: ', response.status);
+            console.log(`Response status code: ${response.status}`);
             result = response.json();
         })
         .catch(function (err) {
-            console.log("Unable to fetch:", err);
+            console.log(`Error: ${err}`);
         });
         console.log(result);
 };
