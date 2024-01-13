1e40092acce50993a899b3edfdfaf749ca5cfabf
marcoguastalli <marco.guastalli@gmail.com>
2024-01-13 19:48:07 +0100

    links

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # marcoguastalli.github.io
 My personal website hosted in github
-
-# 1st Time Build
-npm install
-
-# Build
-webpack
-webpack --mode=development
-webpack --mode=production
--- src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    bitcoin_price_api_url: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd',
-    bitcoin_block_count_api_url: 'https://blockchain.info/q/getblockcount'
-}
